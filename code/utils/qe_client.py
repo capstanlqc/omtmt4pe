849c778e2128510596bcc09754d476ea94a4f997
Manuel Souto Pico <m.soutopico@gmail.com>
2024-08-14 23:26:04 +0200

    Commented out data example

--- code/utils/qe_client.py
+++ code/utils/qe_client.py
@@ -4,20 +4,22 @@ import requests
 
 url = "http://127.0.0.1:8000/api/scores"
 
-data = [
-    {
-        "src": "How to Demonstrate Your Strategic Thinking Skills",
-        "mt": "Cómo demostrar su capacidad de pensamiento estratégico",
-    },
-    {
-        "src": "Why is Accuracy important in the workplace?",
-        "mt": "¿Por qué es importante la precisión en el trabajo",
-    },
-    {
-        "src": "When faced with a large amount of analysis ask for support setting up a team to approach the issue in different ways.",
-        "mt": "Cuando se enfrente a una gran cantidad de análisis, pida ayuda para crear un equipo que aborde la cuestión de diferentes maneras.",
-    },
-]
+# data example:
+
+# data = [
+#     {
+#         "src": "How to Demonstrate Your Strategic Thinking Skills",
+#         "mt": "Cómo demostrar su capacidad de pensamiento estratégico",
+#     },
+#     {
+#         "src": "Why is Accuracy important in the workplace?",
+#         "mt": "¿Por qué es importante la precisión en el trabajo",
+#     },
+#     {
+#         "src": "When faced with a large amount of analysis ask for support setting up a team to approach the issue in different ways.",
+#         "mt": "Cuando se enfrente a una gran cantidad de análisis, pida ayuda para crear un equipo que aborde la cuestión de diferentes maneras.",
+#     },
+# ]
 
 
 def prepare_data(bitexts):
